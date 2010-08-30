ea8c4e5c8d0fb6f314c992f534cf98c7ab7dfc83
Daniel Brockman <daniel@gointeractive.se>
2010-08-30 18:25:05 +0200

    Fix bug in asautotest.sh.

--- asautotest.sh
+++ asautotest.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if [ $ASSPEC_SRC = "" ] ; then
+if [ "x$ASSPEC_SRC" == x ] ; then
   echo 'Please set $ASSPEC_SRC to the source directory of ASSpec:'
-  echo '  cd ~'
-  echo '  git clone git@github.com:dbrock/asspec.git'
+  echo '  git clone git://github.com/dbrock/asspec.git ~/asspec'
   echo '  export ASSPEC_SRC=~/asspec/source'
   exit -1
 fi
