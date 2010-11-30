6ebc62bff92cbc706ac614c5a931ad8017511892
Daniel Brockman <daniel@gointeractive.se>
2010-11-30 23:19:15 +0100

    Remove trailing whitespace in goplayer.js.

--- bin/goplayer.js
+++ bin/goplayer.js
@@ -3,13 +3,13 @@ function block(callback) { callback() }
 var goplayer = {}
 
 block(function () {
-  goplayer.available = function () { return true } 
+  goplayer.available = function () { return true }
 
   function getTimestamp(date)
   {
     function pad(number, places)
     { return (number / Math.pow(10, places)).toFixed(places).substring(2) }
-  
+
     return "<span class='time'>"
       + pad(date.getHours(), 2) + ":"
       + pad(date.getMinutes(), 2) + ":"
@@ -17,26 +17,26 @@ block(function () {
       + pad(date.getMilliseconds(), 3)
       + "</span>"
   }
-  
+
   var lastTime
 
   goplayer.log = function (message)
   {
     var li = document.createElement("li")
     var span = document.createElement("span")
-  
+
     var currentTime = new Date
-  
+
     if (lastTime && currentTime - lastTime > 1000)
       li.className = "first"
-  
+
     li.innerHTML = getTimestamp(currentTime)
     span.appendChild(document.createTextNode(message))
-  
-    var zeroWidthSpace = String.fromCharCode(0x200b) //"​"
+
+    var zeroWidthSpace = String.fromCharCode(0x200b) // "​"
     var valueHTML = "<span class='value'>$1</span>"
     var errorHTML = "<span class='error'>$1</span>"
-  
+
     span.innerHTML = span.innerHTML
       .replace(/\/(\w)/g, "/" + zeroWidthSpace + "$1")
       .replace(/(“.*?”)/g, valueHTML)
@@ -44,15 +44,15 @@ block(function () {
       .replace(/(\d+×\d+)/g, valueHTML)
       .replace(/(\d+(\.\d+)?(s|(k|M|G)?(bps|B)))/g, valueHTML)
       .replace(/(^Error: .*)/g, errorHTML)
-  
+
     li.appendChild(span)
-  
+
     document.getElementById("log").appendChild(li)
-  
+
     var container = document.getElementById("log-container")
-  
+
     container.scrollTop = container.scrollHeight
-  
+
     lastTime = currentTime
   }
 })
