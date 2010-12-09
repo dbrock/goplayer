88581b62e05a15967d58c8a1fb1fc2bac6fa6669
Daniel Brockman <daniel@gointeractive.se>
2010-12-09 20:25:14 +0100

    Load swfobject.js automatically.

--- js-src/goplayer.js
+++ js-src/goplayer.js
@@ -1,68 +1,84 @@
-// You might want to customize these.
-var goplayer_enable_by_default = true
-var goplayer_swf_url = "goplayer.swf"
-var goplayer_params = { allowfullscreen: true, allowscriptaccess: true }
+function goplayer_initialize() {
+  if (swfobject.hasFlashPlayerVersion(goplayer_flash_version))
+    swfobject.addDomLoadEvent(function () {
+      run()
 
-var goplayer_enable_class_name = "goplayer"
-var goplayer_disable_class_name = "no-goplayer"
-var goplayer_flash_version = "9.0.0"
-var goplayer_express_install_swf_url = null
+      function run() {
+        var elements = document.getElementsByTagName("video")
 
-if (typeof swfobject === "undefined")
-  alert("Please load swfobject.js before goplayer.js.")
-else if (swfobject.hasFlashPlayerVersion(goplayer_flash_version))
-  swfobject.addDomLoadEvent(function () {
-    run()
-
-    function run() {
-      var elements = document.getElementsByTagName("video")
-      
-      for (var i = 0; i < elements.length; ++i)
-        if (is_goplayer_element(elements[i]))
-          install_player(elements[i])
-    }
-
-    function is_goplayer_element(element) {
-      var classes = element.className.split(/\s+/)
-
-      return goplayer_enable_by_default
-        ? classes.indexOf(goplayer_disable_class_name) === -1
-        : classes.indexOf(goplayer_enable_class_name) !== -1
-    }
-
-    function get_data_attribute(attribute, result) {
-      result[attribute.name.substring("data-".length)] = attribute.value
-    }
-
-    function get_dataset(element, result) {
-      for (var i = 0; i < element.attributes.length; ++i)
-        if (element.attributes[i].name.indexOf("data-") === 0)
-          get_data_attribute(element.attributes[i], result)
-    }
-
-    function install_player(element) {
-      var div = document.createElement("div")
-      var id = "goplayer-" + Math.random()
-      var width = element.offsetWidth
-      var height = element.offsetHeight      
-      var flashvars = {
-        src: element.src,
-        autoplay: element.autoplay,
-        loop: element.loop
+        for (var i = 0; i < elements.length; ++i)
+          if (is_goplayer_element(elements[i]))
+            install_player(elements[i])
       }
 
-      get_dataset(element, flashvars)
+      function is_goplayer_element(element) {
+        var classes = element.className.split(/\s+/)
 
-      div.id = id
-      element.parentNode.replaceChild(div, element)
+        return goplayer_enable_by_default
+          ? classes.indexOf(goplayer_disable_class_name) === -1
+          : classes.indexOf(goplayer_enable_class_name) !== -1
+      }
 
-      swfobject.embedSWF
-        (goplayer_swf_url,
-         id,
-         width, height,
-         goplayer_flash_version,
-         goplayer_express_install_swf_url,
-         flashvars,
-         goplayer_params)
-    }
+      function get_data_attribute(attribute, result) {
+        result[attribute.name.substring("data-".length)] = attribute.value
+      }
+
+      function get_dataset(element, result) {
+        for (var i = 0; i < element.attributes.length; ++i)
+          if (element.attributes[i].name.indexOf("data-") === 0)
+            get_data_attribute(element.attributes[i], result)
+      }
+
+      function install_player(element) {
+        var div = document.createElement("div")
+        var id = "goplayer-" + Math.random()
+        var width = element.offsetWidth
+        var height = element.offsetHeight
+        var flashvars = {
+          src: element.src,
+          autoplay: element.autoplay,
+          loop: element.loop
+        }
+
+        get_dataset(element, flashvars)
+
+        div.id = id
+        element.parentNode.replaceChild(div, element)
+
+        swfobject.embedSWF
+          (goplayer_swf_url,
+           id,
+           width, height,
+           goplayer_flash_version,
+           goplayer_express_install_swf_url,
+           flashvars,
+           goplayer_params)
+      }
+    })
+}
+
+(function () {
+  function define(name, value) {
+    if (!(name in window))
+      window[name] = value
+  }
+
+  define("goplayer_enable_by_default", true)
+  define("goplayer_swf_url", "goplayer.swf")
+  define("goplayer_load_swfobject", true)
+  define("goplayer_params", {
+    allowfullscreen: true,
+    allowscriptaccess: true
   })
+
+  define("goplayer_enable_class_name", "goplayer")
+  define("goplayer_disable_class_name", "no-goplayer")
+  define("goplayer_flash_version", "9.0.0")
+  define("goplayer_express_install_swf_url", null)
+
+  if (goplayer_load_swfobject)
+    document.write('<' + 'script src="swfobject.js" ' +
+                   'onload="goplayer_initialize()"></' + 'script>')
+  else
+    goplayer_initialize()
+})()
